3252c6ba94cc03a74d8476aa77c359b79b8671b0
jtacchetti <jack.tacchetti@gmail.com>
2025-10-06 19:45:59 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -4,21 +4,34 @@ from math import sin, pi
 
 GPIO.setmode(GPIO.BCM)
 
-p = 25    		# GPIO pin number
+
+# Setup for PWM 1
+#    1, 2, 3,  4,  5,  6,  7,  8,  9, 10
+p = [2, 3, 4, 17, 27, 14, 15, 18, 23, 24]
+
 f_pwm = 500     # frequency of PWM (Hz)
-f_B = 0.25		# frequency for Brightness (Hz)
+f_B = 0.2		# frequency for Brightness (Hz)
+phi = [i * pi / 11 for i in range(10)]
 
-GPIO.setup(p, GPIO.OUT)
-pwm = GPIO.PWM(p, f_pwm)        # create PWM object
+pwm = [0]*10
+for i in range(10):
+	GPIO.setup(p[i], GPIO.OUT)				# sets all pins to be outputs
+	pwm[i] = GPIO.PWM(p[i], f_pwm)	# create pwm objects in a list
 
+dc = [0]*10
 try:
-  pwm.start(0)             			# initiate PWM object
+  for i in range(10):
+    pwm[i].start(0)
   while True:
     t = time.time()
-    dc = 100*((sin(2*pi*f_B*t))**2)	# B = sin(2pi*f_B*t)^2
-    pwm.ChangeDutyCycle(dc)			# update duty cycle value
+
+    for i in range(10):
+      dc[i] = 100*((sin((2*pi*f_B*t)-phi[i]))**2)
+      pwm[i].ChangeDutyCycle(dc[i])
+
 except KeyboardInterrupt:   # stop gracefully on ctrl-C
   print('\nExiting')
 
-pwm.stop()
+for i in range(10):
+	pwm[i].stop()
 GPIO.cleanup()
