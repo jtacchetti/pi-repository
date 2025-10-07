2f9ba1ec24ec439e7dc7312853b17fbe515fb5da
jtacchetti <jack.tacchetti@gmail.com>
2025-10-06 21:26:28 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -8,12 +8,14 @@ GPIO.setmode(GPIO.BCM)
 # Setup for PWM 1
 #    1, 2, 3,  4,  5,  6,  7,  8,  9, 10
 p = [2, 3, 4, 17, 27, 14, 15, 18, 23, 24]
+inputPin = 25
 
 f_pwm = 500     # frequency of PWM (Hz)
 f_B = 0.2		# frequency for Brightness (Hz)
 phi = [i * pi / 11 for i in range(10)]
 
 pwm = [0]*10
+GPIO.setup(inputPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 for i in range(10):
 	GPIO.setup(p[i], GPIO.OUT)				# sets all pins to be outputs
 	pwm[i] = GPIO.PWM(p[i], f_pwm)	# create pwm objects in a list
@@ -24,9 +26,14 @@ try:
     pwm[i].start(0)
   while True:
     t = time.time()
+    state = GPIO.input(inputPin)	# check pin 25 for direction
+    if state == HIGH:
+      direction = -1
+    else:
+      direction = 1 
 
-    for i in range(10):
-      dc[i] = 100*((sin((2*pi*f_B*t)-phi[i]))**2)
+    for i in range(10):		# calcuate duty cycles and apply them
+      dc[i] = 100*((sin((2*pi*f_B*t)-direction*phi[i]))**2)
       pwm[i].ChangeDutyCycle(dc[i])
 
 except KeyboardInterrupt:   # stop gracefully on ctrl-C
