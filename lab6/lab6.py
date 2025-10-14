72389df88cfeba5d7068c74ba9c83e04b6b261ad
jtacchetti <jack.tacchetti@gmail.com>
2025-10-14 12:58:17 -0400

    Update lab6.py

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -8,20 +8,20 @@ shifter = Shifter(23, 24, 25)
 position = 0 	# initial position at 0
 n = 8 			# 8 LEDs
 delay = 0.05
-
+print(f'Setup data pins')
 try:
 	while True:
 		pattern = 1 << position
 		shifter.shiftByte(pattern)
-		
+		print(f'shifting byte')
 		step_direction  = random.choice([-1,1])
 		new_position = position + step_direction
-
+		print(f'decided on direction')
 		if new_position < 0:
 			new_position = 1
 		elif new_position >= n:
 			new_position = n - 2
-
+		print(f'delaying')
 		position = new_position
 		time.sleep(delay)
 
