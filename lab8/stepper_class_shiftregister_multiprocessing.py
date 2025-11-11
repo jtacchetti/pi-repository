fc5f63ebaede0ea945b7f87f459068be317aeb93
jtacchetti <jack.tacchetti@gmail.com>
2025-11-11 15:08:12 -0500

    Update stepper_class_shiftregister_multiprocessing.py

--- lab8/stepper_class_shiftregister_multiprocessing.py
+++ lab8/stepper_class_shiftregister_multiprocessing.py
@@ -8,7 +8,6 @@
 
 import time
 import multiprocessing
-import random
 from shifter import Shifter   # our custom Shifter class
 
 class Stepper:
@@ -58,23 +57,21 @@ class Stepper:
     def __step(self, dir):
         self.step_state += dir    # increment/decrement the step
         self.step_state %= 8      # ensure result stays in [0,7]
-        mask = 0b1111 << self.shifter_bit_start    # clear bits for this motor
-        pattern = Stepper.seq[self.step_state] << self.shifter_bit_start
-
-        with self.lock:
-            Stepper.shifter_outputs &= ~mask
-            Stepper.shifter_outputs |= pattern
-            self.s.shiftByte(Stepper.shifter_outputs)
+        Stepper.shifter_outputs |= 0b1111<<self.shifter_bit_start
+        Stepper.shifter_outputs &= Stepper.seq[self.step_state]<<self.shifter_bit_start
+        self.s.shiftByte(Stepper.shifter_outputs)
         self.angle += dir/Stepper.steps_per_degree
         self.angle %= 360         # limit to [0,359.9+] range
 
     # Move relative angle from current position:
     def __rotate(self, delta):
+        self.lock.acquire()                 # wait until the lock is available
         numSteps = int(Stepper.steps_per_degree * abs(delta))    # find the right # of steps
         dir = self.__sgn(delta)        # find the direction (+/-1)
         for s in range(numSteps):      # take the steps
             self.__step(dir)
-            time.sleep((Stepper.delay + random.randint(-100,100))/1e6)
+            time.sleep(Stepper.delay/1e6)
+        self.lock.release()
 
     # Move relative angle from current position:
     def rotate(self, delta):
